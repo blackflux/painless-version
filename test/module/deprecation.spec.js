b6fc9c026286a0c30c1877afef6f65d191757fd3
Lukas Siemon <github@blackflux.com>
2020-02-24 14:11:27 -0800

    amend: minor

--- test/module/deprecation.spec.js
+++ test/module/deprecation.spec.js
@@ -5,23 +5,23 @@ const sv = require('../../src/index');
 describe('Testing updateDeprecationHeaders()', {
   timestamp: 1582580024
 }, () => {
-  let update;
+  let testRunner;
   beforeEach(() => {
-    update = (r, { date = new Date(), fn = undefined } = {}) => {
+    testRunner = (r, { date = new Date(), fn = undefined } = {}) => {
       sv.updateDeprecationHeaders(r, { deprecationDate: date, sunsetDurationInDays: 1, onSunsetFn: fn });
       return r;
     };
   });
 
   it('Testing invalid headers present cause assertion error', () => {
-    expect(() => update({ deprecation: 'invalid' }))
+    expect(() => testRunner({ deprecation: 'invalid' }))
       .to.throw('Bad format "invalid" for response header "deprecation" detected');
-    expect(() => update({ sunset: 'invalid' }))
+    expect(() => testRunner({ sunset: 'invalid' }))
       .to.throw('Bad format "invalid" for response header "sunset" detected');
   });
 
   it('Testing headers are created', () => {
-    expect(update({})).to.deep.equal({
+    expect(testRunner({})).to.deep.equal({
       deprecation: 'date="Mon, 24 Feb 2020 21:33:44 GMT"',
       sunset: 'Tue, 25 Feb 2020 21:33:44 GMT'
     });
@@ -32,9 +32,9 @@ describe('Testing updateDeprecationHeaders()', {
     const fn = () => {
       called = true;
     };
-    update({}, { fn });
+    testRunner({}, { fn });
     expect(called).to.equal(false);
-    update({}, {
+    testRunner({}, {
       fn,
       date: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7 * 52 * 30)
     });
@@ -43,12 +43,12 @@ describe('Testing updateDeprecationHeaders()', {
 
   it('Testing headers are overwritten if younger', () => {
     const r = {};
-    update(r);
+    testRunner(r);
     expect(r).to.deep.equal({
       deprecation: 'date="Mon, 24 Feb 2020 21:33:44 GMT"',
       sunset: 'Tue, 25 Feb 2020 21:33:44 GMT'
     });
-    update(r, { date: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7 * 52 * 30) });
+    testRunner(r, { date: new Date(new Date().getTime() - 1000 * 60 * 60 * 24 * 7 * 52 * 30) });
     expect(r).to.deep.equal({
       deprecation: 'date="Mon, 02 Apr 1990 21:33:44 GMT"',
       sunset: 'Tue, 03 Apr 1990 21:33:44 GMT'
@@ -57,12 +57,12 @@ describe('Testing updateDeprecationHeaders()', {
 
   it('Testing headers are not overwritten if older', () => {
     const r = {};
-    update(r);
+    testRunner(r);
     expect(r).to.deep.equal({
       deprecation: 'date="Mon, 24 Feb 2020 21:33:44 GMT"',
       sunset: 'Tue, 25 Feb 2020 21:33:44 GMT'
     });
-    update(r, { date: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7 * 52 * 30) });
+    testRunner(r, { date: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 7 * 52 * 30) });
     expect(r).to.deep.equal({
       deprecation: 'date="Mon, 24 Feb 2020 21:33:44 GMT"',
       sunset: 'Tue, 25 Feb 2020 21:33:44 GMT'
